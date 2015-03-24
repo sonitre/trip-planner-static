747e5147617c8e135263a4d046be580f64b86ccf
Jimmy Farrell <jfarrell22@gmail.com>
2015-03-24 18:48:23 -0400

    Data renders to index

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,38 @@
 var router = require('express').Router();
-
+var models = require('../models');
+var async = require('async');
+var Hotel = models.Hotel;
+var Restaurant = models.Restaurant;
+var ThingToDo = models.ThingToDo;
 
 router.get('/', function(req, res, next){
-	res.render('index', {});
+
+	var getAllData = {
+		Hotel: function(done) {
+			Hotel.find({}, function(err, hotels) {
+				if (err) return next(err);
+				done(null, hotels);
+			});
+		},
+
+		Restaurant: function(done) {
+			Restaurant.find({}, function(err, restaurants) {
+				if (err) return next(err);
+				done(null, restaurants);
+			});
+		},
+
+		ThingToDo: function(done) {
+			ThingToDo.find({}, function(err, thingsToDo) {
+				if (err) return next(err);
+				done(null, thingsToDo);
+			});
+		}
+	};
+
+	async.parallel(getAllData, function(err, results) {
+		res.render('index', results);
+	});
 });
 
 module.exports = router;
